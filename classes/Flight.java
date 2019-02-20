ed7bd38a6ff80d68a22a85d83b748ebd5e988827
Antonio Romero <tonijavi.ar@gmail.com>
2019-02-20 13:18:37 +0100

    Mod clase vuelo Antonio 13:18 20/02/2019

--- classes/Flight.java
+++ classes/Flight.java
@@ -4,34 +4,51 @@ import intefaces.IAirCompany;
 public class Flight{
 
   public String siglas;
-  int hour;
+  String hour;
+  Pilot[] pilots=new Pilot[2];
   Airport origin;
   Airport destiny;
   Plane plane;
   Plane planeUsed;
   Pilot pilot1;
   Pilot pilot2;
+  int basePrice;
+  int duration;
+  String siglasComp;
 
   // Array de azafatas
-  Tripulation [] stewardess=new Tripulation [6];
+  Tripulation[] stewardess=new Tripulation [6];
   int numberPlaces;
 
 
 
 
 
-  boolean addFlight(Airport origin, Airport destiny,Plane plane, Pilot pilot1,Pilot pilot2){
+  boolean addFlight(Airport aero1, Airport aero2,Plane plane, Pilot[] pilots,
+   Tripulation steward1, Tripulation steward2, int basePrice, int duration,
+   String siglasComp, String hour){
+    this.origin=aero1;
+    this.destiny=aero2;
+    this.plane=plane;
+
+    for(i=0;i>1;i++){
+      this.pilots[i]=pilots[i];
+    }
+    this.basePrice=basePrice;
+    this.duration=duration;
+    this.generaSiglas();
 
   }
   void listFlight();
   Flight searchFlight();
   boolean removeFlight();
 
-  public generaSiglas(){
+  public String generaSiglas(){
+    StringBuilder micadena=new StringBuilder(siglasComp);
+        micadena.append(hour);
+        micadena.append(destiny);
 
   }
 
-  StringBuilder micadena=new StringBuilder();
-  micadena.append("");
-  micadena.append(siglasComp);
+
 }
