9cb3644e6e42c3539120222ccab1ded2c4f9b850
Antonio Romero <tonijavi.ar@gmail.com>
2019-02-20 11:09:05 +0100

    Clase vuelo añadida

---
+++ classes/Flight.java
@@ -0,0 +1,30 @@
+package classes;
+import intefaces.IAirCompany;
+
+public class Flight{
+
+  public String siglas;
+  int hour;
+  Airport origin;
+  Airport destiny;
+  Plane planeUsed;
+  Pilot pilot1;
+  Pilot pilot2;
+  // Array de azafatas
+  Tripulation [] stewardess=new Tripulation [6];
+  int numberPlaces;
+
+
+
+
+  boolean addFlight(int h){
+
+  }
+  void listFlight();
+  Flight searchFlight();
+  boolean removeFlight();
+
+  public generaSiglas(){
+
+  }
+}
