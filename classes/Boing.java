72b495daf4853d780b251e1c68674eda89c480b5
Antonio Romero <tonijavi.ar@gmail.com>
2019-02-26 11:30:10 +0100

    11.30 comentado y remodelacion del array. He desarrollado tambien el getFreeSeat y el occupaseat

--- classes/Boing.java
+++ classes/Boing.java
@@ -6,7 +6,7 @@ public class Boing extends Plane{
 	private final int PRECIO = 280000000;
 	private final int AUTONOMIAKM = 8000;
 	private final int CONSUMOLITROKM = 12;
-	private final int NUMERODEFILAS = 40;
+	private final int NUMERODEFILAS = 50;
 	private final int NUMEROCOLUMNAS = 6;
 	private final int NUMEROFILASVIP = 10; //De la 1 a la 5
 	private final int CAPACIDADVEHICULOS = 4;
