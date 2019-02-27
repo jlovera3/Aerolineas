9b7ddea97e587f9fb2b3baf040eb109345085bbd
Jaime Lovera Ulecia <jloveraulecia@gmail.com>
2019-02-27 12:45:37 +0100

    12:45 jaime company y cliente

--- classes/Cliente.java
+++ classes/Cliente.java
@@ -1,7 +1,7 @@
 package proyecto.classes;
 
 public class Cliente{
-	
+
 	private final String DNI_DEF="";
 	private final String NOMBRE_DEF="";
 	private final String APELLIDOS_DEF="";
@@ -13,24 +13,26 @@ public class Cliente{
 	protected String apellidos;
 	protected String fechaNac;
 	protected String nacionalidad;
-	
+
+	public static int amountOfClients=0;
+
 	public Cliente(){
 		this.dni = DNI_DEF;
 		this.nombre = NOMBRE_DEF;
 		this.apellidos = APELLIDOS_DEF;
 		this.fechaNac = FECHA_NAC;
 		this.nacionalidad = NACION_DEF;
-		
+		amountOfClients++;
 	}
-	
+
 	public Cliente(String dni, String n, String a, String f, String nac){
 		this.dni = dni;
 		this.nombre = n;
 		this.apellidos = a;
 		this.fechaNac = f;
 		this.nacionalidad = nac;
-		
+		amountOfClients++;
 	}
-	
-	
+
+
 }
